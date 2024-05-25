3330b56174e40663e519ce67bdbf3dd6198b40d3
Lukas Bechtel <lukas.bechtel@hs-esslingen.de>
2024-05-25 13:27:22 +0200

    GeNESIS v1

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name='HSES-GeNESIS',
+    packages=find_packages(),
+    version='1.0',
+    description='Generator for Network Evaluation Scenarios of Industrial Systems',
+    author='Samuel Mueller',
+    license='MIT',
+)
